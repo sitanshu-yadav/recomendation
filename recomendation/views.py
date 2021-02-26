83fe9fb4a3244896b1e8696a83c87f1018467377
Sitanshu <sitanshuyadav2019@gmail.com>
2021-02-26 15:07:49 +0545

    4 th ch

--- recomendation/views.py
+++ recomendation/views.py
@@ -65,27 +65,28 @@ def Thriller(request):
     return HttpResponse( reindex.to_html())
 def cossim(request):
     user_search=request.GET.get("search")
-    df=pd.read_csv(file)
+    df_title=pd.read_csv(file)
     col_list= ["book_id", "title"]
-    df_onlytitle =  pd.read_csv('FinalData.csv', encoding = "ISO-8859-1", usecols=col_list)
-    df['corpus'] = (pd.Series(df[['authors', 'Genres']].fillna('').values.tolist()).str.join(' '))
+    df_title['corpus'] = (pd.Series(df_title[['authors', 'Genres']].fillna('').values.tolist()).str.join(' '))
     tf_corpus = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
-    tfidf_matrix_corpus = tf_corpus.fit_transform(df['corpus'])
+    tfidf_matrix_corpus = tf_corpus.fit_transform(df_title['corpus'])
     cosine_sim_corpus = linear_kernel(tfidf_matrix_corpus, tfidf_matrix_corpus)
-    titles = df['title']
-    indices = pd.Series(df.index, index=df['title'])
-    def corpus_recommendations(title):
-        idx = indices[title]
-        sim_scores = list(enumerate(cosine_sim_corpus[idx]))
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-        sim_scores = sim_scores[1:21]
-        book_indices = [i[0] for i in sim_scores]
-        return titles.iloc[book_indices]
-    rec=corpus_recommendations("search")
-    reindex=rec.reset_index(drop=True)
+
+    titles = df_title['title']
+    indices = pd.Series(df_title.index, index=df_title['title'])
+
+    idx = indices[user_search]
+    sim_scores = list(enumerate(cosine_sim_corpus[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:21]
+    book_indices = [i[0] for i in sim_scores]
+    rec=titles.iloc[book_indices]
+    rec2=pd.Series.to_frame(rec)
+    reindex=rec2.reset_index(drop=True)
     return HttpResponse( reindex.to_html())
 def svd(request):
     df=pd.read_csv(file)
     rec=df.loc[df["Genres"]=="Thriller",["book_id","title","authors"]]
     reindex=rec.reset_index(drop=True)
     return HttpResponse( reindex.to_html())
+    
